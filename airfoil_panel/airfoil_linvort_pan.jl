a37b50d77ace9017ae0d886bb069615e8aa63efc
mlau3468 <marklausas@gmail.com>
2020-12-15 17:44:23 -0500

    debug

--- airfoil_panel/airfoil_linvort_pan.jl
+++ airfoil_panel/airfoil_linvort_pan.jl
@@ -3,7 +3,7 @@ include("airfoil_util.jl")
 function linVort(mu1, mu2, p1, p2, p)
     r1 = ptDist(p1, p)
     r2 = ptDist(p2, p)
-    theta = -atan(p2[2]-p1[2], p2[1]-p1[1]);
+    theta = -atan(p2[2]-p1[2], p2[1]-p1[1])
     p_new = coordRot2D(p, theta, p1)
 
     x1 = 0
@@ -13,35 +13,28 @@ function linVort(mu1, mu2, p1, p2, p)
     z = p_new[2]
     theta1 = atan(z,x)
     theta2 = atan(z, x-x2)
-
     # first panel point
     up_a = z/2/pi*(-mu1)/(x2-x1)*log(r2/r1) + (mu1*(x2-x1)-mu1*(x-x1)) /2/pi/(x2-x1) * (theta2-theta1)
-    wp_a = -(mu1*(x2-x1)-mu1*(x-x1)) /2/pi/(x2-x1) * log(r1/r2) + z/2/pi*(-mu1)/(x2-x1) * ((x2-x1)/z+theta2-theta1)
-
+    wp_a = -(mu1*(x2-x1)-mu1*(x-x1)) /2/pi/(x2-x1) * log(r1/r2) + z/2/pi*(-mu1)/(x2-x1) * ((x2-x1)/z-(theta2-theta1))
+    # katz plotkin pg 303. note error in wp term. a + is actually a -
     # second panel point
     up_b = z/2/pi*(mu2)/(x2-x1)*log(r2/r1) + mu2*(x-x1)/2/pi/(x2-x1) * (theta2-theta1)
-    wp_b = -(mu2*(x-x1))/2/pi/(x2-x1)*log(r1/r2) + z/2/pi*(mu2)/(x2-x1) * ((x2-x1)/z+theta2-theta1)
+    wp_b = -(mu2*(x-x1))/2/pi/(x2-x1)*log(r1/r2) + z/2/pi*(mu2)/(x2-x1) * ((x2-x1)/z-(theta2-theta1))
 
     vel_a = coordRot2D([up_a, wp_a], -theta, [0,0])
     vel_b = coordRot2D([up_b, wp_b], -theta, [0,0])
 
     vel = vel_a .+ vel_b
-    if isnan(vel[1]) || isnan(vel[2])
-        println()
-        println(p1)
-        println(p2)
-        println(p)
-        println("----")
-    end
     
     return [vel'; vel_a'; vel_b']
 
 end
-
+println("----")
 pan_pts = readAF("airfoil.csv", true)
 writeAF(pan_pts, "airfoil.dat")
 #pan_pts = repanel(pan_pts, 50, 1.0, true)
 pan_pts, c_pts, thetas, norms, tangents, dists = procPanels(pan_pts)
+writedlm("CO.csv", c_pts, ',')
 # conditions
 U = 1
 chord = 1
@@ -72,6 +65,38 @@ for i = 1:num_pan
 
             uw = uw_a .+ uw_b
         end
+        if isnan(uw[1]) || isnan(uw[2])
+            println([i,j])
+            p = c_pts[i,:]
+            p2 = pan_pts[j,3:4]
+            p1 = pan_pts[j,1:2]
+            theta = -atan(p2[2]-p1[2], p2[1]-p1[1])
+            p_new = coordRot2D(p, theta, p1) #good
+
+            x = p_new[1]
+            z = p_new[2]
+            x2 = ptDist(p1, p2)
+            x1 = 0
+            theta1 = atan(z,x)
+            theta2 = atan(z, x-x2)
+            mu1 = 1
+            mu2 = 1
+            r1 = ptDist(p1, p)
+            r2 = ptDist(p2, p)
+            # first panel point
+            up_a = z/2/pi*(-mu1)/(x2-x1)*log(r2/r1) + (mu1*(x2-x1)-mu1*(x-x1)) /2/pi/(x2-x1) * (theta2-theta1)
+            wp_a = -(mu1*(x2-x1)-mu1*(x-x1)) /2/pi/(x2-x1) * log(r1/r2) + z/2/pi*(-mu1)/(x2-x1) * ((x2-x1)/z-(theta2-theta1))
+            # katz plotkin pg 303. note error in wp term. a + is actually a -
+            # second panel point
+            up_b = z/2/pi*(mu2)/(x2-x1)*log(r2/r1) + mu2*(x-x1)/2/pi/(x2-x1) * (theta2-theta1)
+            wp_b = -(mu2*(x-x1))/2/pi/(x2-x1)*log(r1/r2) + z/2/pi*(mu2)/(x2-x1) * ((x2-x1)/z-(theta2-theta1))
+            println([up_a, wp_a, up_b, wp_b,z])
+            vel_a = coordRot2D([up_a, wp_a], -theta, [0,0])
+            vel_b = coordRot2D([up_b, wp_b], -theta, [0,0])
+        
+            println([vel_a, vel_b])
+            
+        end
         A[i,j] = uw'norms[i,:]
     end
 end
@@ -81,7 +106,6 @@ A[end, 1] = 1
 A[end, end] = 1
 RHS[end] = 0
 
-display(A)
 #=
 # remove one panel
 remIdx = 30
@@ -97,7 +121,7 @@ thetas = thetas[keep_idx[1:end-1]]
 pan_pts = pan_pts[keep_idx[1:end-1], :]
 dists = dists[keep_idx[1:end-1]]
 =#
-
+writedlm("A.csv", A, ',')
 mu = A\RHS
 
 #=
@@ -138,7 +162,7 @@ fs = ps.*dists.*(-1).*norms[:,2]
 cl = sum(fs)./0.5./chord./rho./U.^2
 println("CL: $cl")
 
-
+#=
 # velocities
 pan_vels = zeros(num_pan,2)
 for i = 1:num_pan
@@ -156,7 +180,7 @@ end
 cp = 1 .- (pan_vels[:,1].^2 + pan_vels[:,2].^2) ./ U.^2
 im = plot(c_pts[:,1], cp, yflip=true)
 display(im)
-
+=#
 ps = cp.*0.5.*rho.*U.^2
 fs = ps.*dists.*(-1).*norms[:,2]
 cl = sum(fs)./0.5./chord./rho./U.^2
