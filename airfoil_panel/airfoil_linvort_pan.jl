53073b1741db0a02df8d4c1bc7229b4e554549ce
Mark Lau <marklausas@gmail.com>
2020-12-16 01:39:38 -0500

    debug

--- airfoil_panel/airfoil_linvort_pan.jl
+++ airfoil_panel/airfoil_linvort_pan.jl
@@ -11,6 +11,8 @@ function linVort(mu1, mu2, p1, p2, p)
 
     x = p_new[1]
     z = p_new[2]
+
+
     theta1 = atan(z,x)
     theta2 = atan(z, x-x2)
     # first panel point
@@ -47,6 +49,28 @@ B = zeros(num_pan, num_pan+1) # tangent matrix
 RHS = zeros(num_pan+1)
 u_vec = U .* [cosd(alpha), sind(alpha)]
 
+function linVort2(mu1, mu2, p1, p2, p)
+    theta = -atan(p2[2]-p1[2], p2[1]-p1[1])
+    p_new = coordRot2D(p, theta, p1)
+
+    x1 = 0
+    x2 = ptDist(p1, p2)
+
+    x = p_new[1]
+    z = p_new[2]
+    up_a=-0.5*(x-x2)/(x2)
+    up_b=0.5*(x)/(x2)
+    wp_a=-0.15916
+    wp_b=0.15916
+
+    vel_a = coordRot2D([up_a, wp_a], -theta, [0,0])
+    vel_b = coordRot2D([up_b, wp_b], -theta, [0,0])
+
+    vel = vel_a .+ vel_b
+    return [vel'; vel_a'; vel_b']
+
+end
+
 for i = 1:num_pan
     RHS[i] = -u_vec'norms[i, :]
     for j = 1:num_pan + 1
@@ -67,6 +91,64 @@ for i = 1:num_pan
         end
         A[i,j] = uw'norms[i,:]
         B[i,j] = uw'tangents[i,:]
+    end
+end
+
+# fix b
+
+for i = 1:num_pan
+    for j = 1:num_pan
+        mu1 = 1
+        mu2 = 1
+        p = c_pts[i,:]
+        p1 = pan_pts[j,1:2]
+        p2 = pan_pts[j,3:4]
+        r1 = ptDist(p1, p)
+        r2 = ptDist(p2, p)
+        theta = -atan(p2[2]-p1[2], p2[1]-p1[1])
+        p_new = coordRot2D(p, theta, p1)
+    
+        x1 = 0
+        x2 = ptDist(p1, p2)
+    
+        
+        x = p_new[1]
+        z = p_new[2]
+
+        if i == j
+            up_a=-0.5*(x-x2)/(x2)
+            up_b=0.5*(x)/(x2)
+            wp_a=-0.15916
+            wp_b=0.15916
+
+            
+            vel_a = coordRot2D([up_a, wp_a], -theta, [0,0])
+            vel_b = coordRot2D([up_b, wp_b], -theta, [0,0])
+        else
+            theta1 = atan(z,x)
+            theta2 = atan(z, x-x2)
+            # first panel point
+            up_a = z/2/pi*(-mu1)/(x2-x1)*log(r2/r1) + (mu1*(x2-x1)-mu1*(x-x1)) /2/pi/(x2-x1) * (theta2-theta1)
+            wp_a = -(mu1*(x2-x1)-mu1*(x-x1)) /2/pi/(x2-x1) * log(r1/r2) + z/2/pi*(-mu1)/(x2-x1) * ((x2-x1)/z-(theta2-theta1))
+            # katz plotkin pg 303. note error in wp term. a + is actually a -
+            # second panel point
+            up_b = z/2/pi*(mu2)/(x2-x1)*log(r2/r1) + mu2*(x-x1)/2/pi/(x2-x1) * (theta2-theta1)
+            wp_b = -(mu2*(x-x1))/2/pi/(x2-x1)*log(r1/r2) + z/2/pi*(mu2)/(x2-x1) * ((x2-x1)/z-(theta2-theta1))
+        
+            vel_a = coordRot2D([up_a, wp_a], -theta, [0,0])
+            vel_b = coordRot2D([up_b, wp_b], -theta, [0,0])
+        end
+
+        if j == 1
+            B[i,1] = vel_a'tangents[i,:]
+            global holdb = vel_b'tangents[i,:]
+        elseif j == num_pan
+            B[i,num_pan] = vel_a'tangents[i,:]+holdb
+            B[i,num_pan+1] = vel_b'tangents[i,:]
+        else
+            B[i,j] = vel_a'tangents[i,:]+holdb
+            global holdb=vel_b'tangents[i,:]
+        end
     end
 end
 
@@ -93,7 +175,10 @@ println("CL: $cl")
 pan_vels = zeros(num_pan,2)
 for i = 1:num_pan
     pan_vels[i,:] = u_vec
-    
+    #=
+    temp = (mu[i] + mu[i+1])/4
+    pan_vels[i,:] = pan_vels[i,:] + coordRot2D([temp, 0], thetas[i], [0,0])
+    =#
     for j = 1:num_pan
         res = linVort(mu[j], mu[j+1], pan_pts[j,1:2], pan_pts[j,3:4], c_pts[i,:])
         pan_vels[i,:] = pan_vels[i,:] + res[1,:]
@@ -127,12 +212,13 @@ display(im)
 
 writedlm("cp.csv", cp, ',')
 writedlm("A.csv", A, ',')
+writedlm("B.csv", B, ',')
 writedlm("G.csv", mu, ',')
 writedlm("RHS.csv", RHS, ',')
 writedlm("thetas.csv", thetas, ',')
 
 
-
+#B = readdlm("B.txt")
 # fortran implementation
 vs = zeros(num_pan)
 cp = zeros(num_pan)
