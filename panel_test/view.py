7c974c5c5b448ea0b73c8e3bac6fe9d89988ac59
mlau3468 <marklausas@gmail.com>
2021-01-26 04:30:40 -0500

    transfer

--- panel_test/view.py
+++
@@ -1,9 +0,0 @@
-import pymesh
-from dust.tools.DUST_utils import *
-
-from numpy import genfromtxt
-ee = genfromtxt('ee.csv', delimiter=',')
-print(ee)
-rr = genfromtxt('rr.csv', delimiter=',')
-print(rr)
-view_ee_rr(ee,rr)
