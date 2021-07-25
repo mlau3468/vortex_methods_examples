541f0d4009b39195b25b8257cd908fd2f1c36023
Mark Lau <marklausas@gmail.com>
2021-07-25 01:57:03 -0400

    added ibl methods

---
+++ airfoil_vortexpanel/airfoil_dub_pan.jl
@@ -0,0 +1,134 @@
+include("airfoil_util.jl")
+
+function constDubPan(mu, p1, p2, p)
+    #mu = doublet strength
+    #p = point to calc velocity
+    #p1 = start point of doublet panel
+    #p2 = end point of doublet panel
+
+    if p2 === nothing # wake
+        theta = 0
+        x1 = 0
+        x = p[1] - p1[1]
+        z = p[2] - p1[2]
+        r_2 = (x-x1)^2+z^2
+        u = mu/2/pi/r_2 * z
+        w = -mu/2/pi / r_2 * x
+    else
+        theta = -atan(p2[2]-p1[2], p2[1]-p1[1]);
+        p_new = coordRot2D(p, theta, p1)
+
+        x1 = 0
+        x2 = ptDist(p1, p2)
+
+        x = p_new[1]
+        z = p_new[2]
+        if abs(z) < 0.00001 && x1<x && x<x2 # Influence on itself
+            u = 0
+            w = -mu/2/pi * (1/(x-x1) - 1/(x-x2))
+        else
+            u = mu/2/pi * (z/((x-x1)^2+z^2) - z/((x-x2)^2+z^2))
+            w = -mu/2/pi * ((x-x1)/((x-x1)^2+z^2) - (x-x2)/((x-x2)^2+z^2))
+        end
+    end
+    # transform back
+    vel = [u, w]
+    vel = coordRot2D(vel, -theta, [0,0])
+    return vel
+
+end
+
+function airfoil_constDub(pan_pts, alpha)
+pan_pts, c_pts, thetas, norms, tangents, dists = procPanels(pan_pts)
+# append a panel for the wake
+pan_pts = [pan_pts; [pan_pts[end,3], pan_pts[end,4], NaN, NaN]']
+
+# conditions
+U = 1
+chord = 1
+rho = 1.225
+
+# Initialize solver matrix
+num_pan = size(pan_pts,1) -1
+A = zeros(num_pan+1, num_pan+1)
+RHS = zeros(num_pan+1)
+u_vec = U .* [cosd(alpha), sind(alpha)]
+
+# Influence Matrix
+for i in 1:num_pan
+    RHS[i] =- u_vec'norms[i, :]
+    for j in 1:num_pan+1
+        if j == num_pan+1 # wake, last panel
+            uw = constDubPan(1, pan_pts[j,1:2], nothing, c_pts[i,:])
+        else
+            uw = constDubPan(1, pan_pts[j,1:2], pan_pts[j,3:4], c_pts[i, :])
+        end
+        A[i,j] = uw'norms[i,:]
+     end
+end
+
+# Kutta condition
+A[end, 1] = 1
+A[end, end] = 1
+A[end, end-1] = -1
+RHS[end] = 0
+
+# remove one panel
+remIdx = Int(round(size(pan_pts,1)/2))
+num_pan = num_pan -1
+keep_idx = [1:remIdx-1; remIdx+1:num_pan+2]
+
+RHS = RHS[keep_idx]
+A = A[keep_idx, keep_idx]
+c_pts = c_pts[keep_idx[1:end-1], :]
+norms = norms[keep_idx[1:end-1],:]
+tangents = tangents[keep_idx[1:end-1],:]
+thetas = thetas[keep_idx[1:end-1]]
+pan_pts = pan_pts[keep_idx, :]
+dists = dists[keep_idx[1:end-1]]
+
+mu = A\RHS
+
+pan_vels = zeros(num_pan, 2)
+# velocities at each panel
+for i in 1:num_pan
+    pan_vels[i,:] = u_vec
+    # perterbation velocity of panel on itself
+    if i == 1  # First panel
+        v_pan = 0.5*(mu[i+1]-mu[i])/ptDist(c_pts[i,:], c_pts[i+1,:])
+    elseif i == num_pan  # last panel
+        v_pan = 0.5*(mu[i]-mu[i-1])/ptDist(c_pts[i-1,:], c_pts[i,:])
+    else
+         v_pan = 0.5*(mu[i+1]-mu[i-1])/ptDist(c_pts[i-1,:], c_pts[i+1,:])
+    end
+    pan_vels[i, :] = pan_vels[i,:] +  coordRot2D([v_pan, 0], thetas[i], [0,0])
+    
+    # influence of other panels
+    for j in 1:num_pan+1
+        if j == num_pan+1 # wake, last panel
+            uw = constDubPan(mu[j], pan_pts[j,1:2], nothing, c_pts[i,:])
+        else
+            uw = constDubPan(mu[j], pan_pts[j,1:2], pan_pts[j,3:4], c_pts[i, :])
+        end
+        pan_vels[i, :] = pan_vels[i,:] + uw
+     end
+end
+
+cp = 1 .- (pan_vels[:,1].^2 + pan_vels[:,2].^2) ./ U.^2
+cpPlot = plot(c_pts[:,1], cp, yflip=true)
+
+ps = cp.*0.5.*rho.*U.^2
+fs = ps.*dists.*(-1).*norms[:,2]
+cl = sum(fs)./0.5./chord./rho./U.^2
+return [cl, cpPlot]
+end
+
+
+# Testing
+pan_pts = readAF("airfoil.csv", true)
+writeAF(pan_pts, "airfoil.dat")
+#pan_pts = repanel(pan_pts, 50, 1, true)
+cl, cpPlot = airfoil_constDub(pan_pts, alpha)
+
+println("CL: $cl")
+display(cpPlot)
---
+++ airfoil_vortexpanel/airfoil_linvort_pan.jl
@@ -0,0 +1,123 @@
+include("airfoil_util.jl")
+
+function linVortPan(mu1, mu2, p1, p2, p, coloc=false)
+    r1 = ptDist(p1, p)
+    r2 = ptDist(p2, p)
+    theta = -atan(p2[2]-p1[2], p2[1]-p1[1])
+    p_new = coordRot2D(p, theta, p1)
+
+    x1 = 0
+    x2 = ptDist(p1, p2)
+
+    x = p_new[1]
+    z = p_new[2]
+
+    theta1 = atan(z,x)
+    theta2 = atan(z, x-x2)
+
+    tol = 1e-8
+    # if on panel itself
+    if (z < tol && abs(theta1) < tol && abs((abs(theta2)-pi) < tol)) || coloc
+        up_a=-0.5*(x-x2)/(x2)
+        up_b=0.5*(x)/(x2)
+        wp_a=-1/2/pi
+        wp_b=1/2/pi
+    else
+        # first panel point
+        up_a = z/2/pi*(-mu1)/(x2-x1)*log(r2/r1) + (mu1*(x2-x1)-mu1*(x-x1)) /2/pi/(x2-x1) * (theta2-theta1)
+        wp_a = -(mu1*(x2-x1)-mu1*(x-x1)) /2/pi/(x2-x1) * log(r1/r2) + z/2/pi*(-mu1)/(x2-x1) * ((x2-x1)/z-(theta2-theta1))
+        # katz plotkin pg 303. note error in wp term. a + is actually a -
+        # second panel point
+        up_b = z/2/pi*(mu2)/(x2-x1)*log(r2/r1) + mu2*(x-x1)/2/pi/(x2-x1) * (theta2-theta1)
+        wp_b = -(mu2*(x-x1))/2/pi/(x2-x1)*log(r1/r2) + z/2/pi*(mu2)/(x2-x1) * ((x2-x1)/z-(theta2-theta1))
+    end
+    uw_a = coordRot2D([up_a, wp_a], -theta, [0,0])
+    uw_b = coordRot2D([up_b, wp_b], -theta, [0,0])
+
+    uw = uw_a .+ uw_b
+    
+    return [uw'; uw_a'; uw_b']
+
+end
+
+function airfoil_linVort(pan_pts, alpha)
+pan_pts, c_pts, thetas, norms, tangents, dists = procPanels(pan_pts)
+# conditions
+U = 1
+chord = 1
+rho = 1.225
+
+# Initialize solver matrix
+num_pan = size(pan_pts,1)
+A = zeros(num_pan+1, num_pan+1) # normal matrix
+B = zeros(num_pan, num_pan+1) # tangent matrix
+RHS = zeros(num_pan+1)
+u_vec = U .* [cosd(alpha), sind(alpha)]
+
+for i = 1:num_pan
+    RHS[i] = -u_vec'norms[i, :]
+    for j = 1:num_pan
+        if i == j 
+            res = linVortPan(1, 1, pan_pts[j,1:2], pan_pts[j,3:4], c_pts[i,:], true)
+        else
+            res = linVortPan(1, 1, pan_pts[j,1:2], pan_pts[j,3:4], c_pts[i,:])
+        end 
+        
+        uw_a = res[2,:]
+        uw_b = res[3,:]
+        
+        if j == 1
+            A[i,j] = uw_a'norms[i,:]
+            B[i,j] = uw_b'tangents[i,:]
+        elseif j == num_pan
+            A[i,j] = uw_a'norms[i,:] + holda
+            B[i,j] = uw_b'tangents[i,:] + holdb
+            A[i,num_pan+1] = uw_b'norms[i,:]
+            B[i,num_pan+1] = uw_b'tangents[i,:]
+        else
+            A[i,j] = uw_a'norms[i,:] + holda
+            B[i,j] = uw_b'tangents[i,:] + holdb
+        end
+
+        global holda = uw_b'norms[i,:]
+        global holdb = uw_b'tangents[i,:]
+    end
+end
+
+# Kutta condition
+A[end, 1] = 1
+A[end, end] = 1
+RHS[end] = 0
+
+mu = A\RHS
+
+# lift by kutta jokowski
+ls = zeros(num_pan)
+for i = 1:num_pan
+    ls[i] = rho*U.*(mu[i] + mu[i+1])/2*dists[i]
+end
+lift = sum(ls)
+cl = lift/0.5/rho/U^2/chord
+
+# get cp
+vs = zeros(num_pan)
+cp = zeros(num_pan)
+for i = 1:num_pan
+    vs[i] = B[i,:]'mu + u_vec'tangents[i,:]
+    cp[i] = 1-vs[i]^2/U^2
+end
+cpPlot = plot(c_pts[:,1], cp, yflip=true)
+
+return [cl, cpPlot]
+end
+
+
+
+# Testing
+pan_pts = readAF("airfoil.csv", true)
+writeAF(pan_pts, "airfoil.dat")
+#pan_pts = repanel(pan_pts, 80, 0.75, true)
+
+cl, cpPlot = airfoil_linVort(pan_pts, 5)
+display(cpPlot)
+println("CL: $cl")
