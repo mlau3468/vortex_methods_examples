f98320e12a07b1d63bcfd8d6a95b62a76629c64c
Mark Lau <marklausas@gmail.com>
2021-07-27 03:50:03 -0400

    fdiff

--- rotor_opt/forwardDiffTest.jl
+++ rotor_opt/forwardDiffTest.jl
@@ -8,24 +8,26 @@ include("ibl.jl")
 
 function test!(output,nacaNum)
     pts = genNACA(nacaNum, 75)
-    res = airfoilCalc(pts, 30, 2)
-    output[1] = res[1]
-    output[2] = res[2]
+    alphas = 1:2
+    for a = 1:length(alphas)
+        res = airfoilCalc(pts, 30, a)
+        output[a,1] = res[1]
+        output[a,2] = res[2]
+    end
     return output
 end
 
 inputShape = [4, 1]
-outputShape = [2, 1]
+outputShape = [2, 2]
 gradShape = [prod(outputShape), prod(inputShape)]
 
 inputs = [2.0, 4.0, 1.0, 2.0]
-outputs = Array{Float64}(undef, outputShape...)
-gradresults = rand(gradShape...)
+outputs = similar(inputs, outputShape...)
+gradresults = similar(inputs, gradShape...)
 
 
 const jconfig = JacobianConfig(test!, outputs, inputs, Chunk{4}())
 
 jacobian!(gradresults,test!, outputs,inputs, jconfig)
-
 display(outputs)
 display(gradresults)
