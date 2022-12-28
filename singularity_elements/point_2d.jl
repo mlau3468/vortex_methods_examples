c09bbfa5d73c5104f091ea6e3a49f81c3ddabafb
Mark Lau <marklausas@gmail.com>
2022-12-27 19:00:49 -0500

    added 2d point elements

--- singularity_elements/point_2d.jl
+++ singularity_elements/point_2d.jl
@@ -23,7 +23,7 @@ function vel_point_source_2d(p0::AbstractVector{<:Real}, p::AbstractVector{<:Rea
 end
 
 function vel_point_source_2d_ad(p0::AbstractVector{<:Real}, p::AbstractVector{<:Real})
-    # Velocity induced by unit strength point source by autodifferentiation
+    # Velocity induced by unit strength point source by differentiation of potential
     return ForwardDiff.gradient((x) -> pot_point_source_2d(p0, x), p)
 end
 
@@ -52,10 +52,34 @@ function vel_point_doublet_2d(p0::AbstractVector{<:Real}, p::AbstractVector{<:Re
 end
 
 function vel_point_doublet_2d_ad(p0::AbstractVector{<:Real}, p::AbstractVector{<:Real})
-    # Velocity induced by unit strength doublet oriented in z direction
+    # Velocity induced by unit strength doublet oriented in z direction by differentiation of potential
     return ForwardDiff.gradient((x) -> pot_point_doublet_2d(p0, x), p)
 end
 
 function pot_point_vortex_2d(p0::AbstractVector{<:Real}, p::AbstractVector{<:Real})
     # Potential induced by unit strength vortex
+    # pg 232 eq. 10.8
+    x = p[1]
+    z = p[2]
+    x0 = p0[1]
+    z0 = p0[2]
+    return -1/(2*pi)*atan(z-z0, x-x0)
+end
+
+function vel_point_vortex_2d(p0::AbstractVector{<:Real}, p::AbstractVector{<:Real})
+    # Velocity induced by unit strength vortex
+    # pg 232 eq 10.9, 10.10
+    x = p[1]
+    z = p[2]
+    x0 = p0[1]
+    z0 = p0[2]
+    r2 = (x-x0)^2 + (z-z0)^2
+    u = 1/(2*pi)*(z-z0)/r2
+    w = -1/(2*pi)*(x-x0)/r2
+    return [u;w]
+end
+
+function vel_point_vortex_2d_ad(p0::AbstractVector{<:Real}, p::AbstractVector{<:Real})
+    # Velocity induced by unit strength vortex by differentiation of potential
+    return ForwardDiff.gradient((x) -> pot_point_vortex_2d(p0, x), p)
 end
--- singularity_elements/test.jl
+++ singularity_elements/test.jl
@@ -1,4 +1,4 @@
 include("singularity_elements.jl")
 
-a = vel_point_doublet_2d([0;0], [1;0])
-b = vel_point_doublet_2d_ad([0;0], [1;0])
+a = vel_point_vortex_2d([0;0], [1;0.5])
+b = vel_point_vortex_2d_ad([0;0], [1;0.5])
